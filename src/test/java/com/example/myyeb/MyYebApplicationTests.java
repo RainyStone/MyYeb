8c6cbb417531eb22bf8c1c0a0e87f79ad2bfd656
xzx <3144790170@qq.com>
2023-06-12 12:58:13 +0800

    first commit

---
+++ src/test/java/com/example/myyeb/MyYebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myyeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyYebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
